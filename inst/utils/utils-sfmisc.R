c652c13c1518818834896e1fe4d1cbd785278ec6
Stefan Fleck <stefan.fleck@statistik.gv.at>
2018-06-11 14:44:23 +0200

    assert_namespace now returns TRUE

--- inst/utils/utils-sfmisc.R
+++ inst/utils/utils-sfmisc.R
@@ -22,10 +22,75 @@ walk <- function(.x, .f, ...){
 
 assert_namespace <- function(x){
   stopifnot(requireNamespace(x, quietly = TRUE))
+  invisible(TRUE)
 }
 
 
 
+
+
+# conditions --------------------------------------------------------------
+
+#' Condition constructor
+#'
+#' A constructur function for conditions, taken from
+#' \url{http://adv-r.had.co.nz/beyond-exception-handling.html}
+#'
+#' @param subclass Subclass to assign to the condition
+#' @param message  message to be passed to the condition
+#' @param call     call passed on to the conditon
+#' @param ...      further list elements to be passed on to the resulting object
+#'
+#' @return a condition object
+#' @noRd
+#'
+#' @examples
+#'
+#' \dontrun{
+#' # Construct a custom condition
+#' malformed_log_entry_error <- function(text) {
+#'   msg <- paste0("Malformed log entry: ", text)
+#'   condition(
+#'     c("malformed_log_entry_entry", "error"),
+#'     message = msg,
+#'     text = text
+#'   )
+#' }
+#'
+#'
+#' # Signal the condition
+#' parse_log_entry <- function(text) {
+#'   if (!well_formed_log_entry(text)) {
+#'     stop(malformed_log_entry_error(text))
+#'    }
+#' }
+#'
+#'
+#' # Handle the condition
+#' tryCatch(
+#'   malformed_log_entry = function(e) NULL,
+#'   parse_log_entry(text)
+#' )
+#' }
+#'
+condition <- function(subclass, message, call = sys.call(-1), ...) {
+  structure(
+    class = c(subclass, "condition"),
+    list(message = message, call = call, ...)
+  )
+}
+
+
+
+
+#' @export
+#' @rdname condition
+error <- function(subclass, message, call = sys.call(-1), ...) {
+  structure(
+    class = c(subclass, "error", 'condition'),
+    list(message = message, call = call, ...)
+  )
+}
 
 # predicates --------------------------------------------------------------
 
@@ -63,6 +128,14 @@ is_integerish <- function(x){
   } else {
     as.integer(x) == x
   }
+}
+
+
+
+
+is_equal_length <- function(...){
+  lengths <- vapply(list(...), length, 1L)
+  identical(length(unique(lengths)), 1L)
 }
 
 
