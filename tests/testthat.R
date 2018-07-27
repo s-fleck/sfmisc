56231f9cf986e97a911b86263ca45a08b6ce4a60
Stefan Fleck <stefan.fleck@statistik.gv.at>
2018-07-27 08:47:54 +0200

    added tests for assert

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(sfmisc)
+
+test_check("sfmisc")
---
+++ tests/testthat/test_assert.R
@@ -0,0 +1,20 @@
+
+test_that("assert handles custom errors", {
+
+  expect_error(assert(FALSE), "is not")
+
+  expect_error(assert(FALSE, "blah"), "blah")
+
+  blah_error <- structure(
+    class = c("blah_error", "error", "condition"),
+    list(message = "blubb", call = call)
+  )
+
+  expect_silent(expect_error(assert(FALSE, blah_error)))
+
+  assert(FALSE, blah_error)
+
+
+
+
+})
